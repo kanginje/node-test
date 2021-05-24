b5ddb4aafec3c67b15eb8139323a3efdc81124a1
kanginje <inje2588@naver.com>
2021-05-24 17:28:16 +0900

    Initial commit

--- app.js
+++ app.js
@@ -36,16 +36,41 @@ app.post('/message', (req, res) => {
 });
 
 
+let users = [
+
+  {
+    id: 1,
+    name: 'alice'
+  },
+
+  {
+    id: 2,
+    name: 'bek'
+  },
+  {
+    id: 3,
+    name: 'chris'
+  }
+]
+
+app.get('/users', (req, res) => {
+   console.log('who get in here/users');
+   res.json(users)
+});
+
 app.post('/post', (req, res) => {
-  var inputData;
-  req.on('data', (data) => {
-    inputData = JSON.parse(data);
-  });
-  req.on('end', () => {
-    console.log(inputData);
-  });
-  res.write("OK!");
-  res.end();
-}); 
+   console.log('who get in here post /users');
+   var inputData;
+   req.on('data', (data) => {
+     inputData = JSON.parse(data);
+   });
+   req.on('end', () => {
+     console.log("user_id : "+inputData.user_id + " , name : "+inputData.name);
+   });
+
+   res.write("OK!");
+   res.end();
+});
+
 
 app.listen(3000, () => console.log('node on 3000'));
