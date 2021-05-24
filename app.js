c41467ea01647acdfab90159406bdea60f74d937
kanginje <inje2588@naver.com>
2021-05-24 15:47:12 +0900

    Initial commit

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/keyboard', (req, res) => {
-  const data = {'type': 'text'}
-  res.json(data);
+  const data1 = {'type': 'text'}
+  res.json(data1);
 });
+
+let data2;
 
 app.post('/message', (req, res) => {
   const question = req.body.userRequest.utterance;
   const goMain = '처음으로';
   
   if (question === '테스트') {
-    const data = {
+    data2 = {
       'version': '2.0',
       'template': {
 	    'outputs': [{
@@ -30,7 +32,7 @@ app.post('/message', (req, res) => {
       }
     }
   }
-  res.json(data);
+  res.json(data2);
 });
 
 app.listen(3000, () => console.log('node on 3000'));
