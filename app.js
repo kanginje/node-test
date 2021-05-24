e555f487f8d57d902806d17d9e52e6bb9bd667e4
kanginje <inje2588@naver.com>
2021-05-24 17:53:04 +0900

    Initial commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/keyboard', (req, res) => {
   const data1 = {'type': 'text'}
