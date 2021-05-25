92dc8f4ef79b49ba0caba6a9782b9579c50f582b
kanginje <inje2588@naver.com>
2021-05-25 16:47:59 +0900

    Initial commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const morgan = require('morgan')
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 
 app.get('/keyboard', (req, res) => {
   const data1 = {'type': 'text'}
@@ -76,10 +78,24 @@ app.post('/post', (req, res) => {
    res.end();
 });
 
-const WebSocket = require('./socket');
+app.use((req, res) => {
 
-const server = app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기 중');
+  console.log('첫 번째 미들웨어 호출 됨');
+  var approve ={'approve_id':'NO','approve_pw':'NO'};
+
+
+  var paramId = req.body.id;
+  var paramPassword = req.body.password;
+  console.log('id : '+paramId+'  pw : '+paramPassword);
+
+  //아이디 일치여부 flag json 데이터입니다.
+  if(paramId == 'test01') approve.approve_id = 'OK';
+  if(paramPassword == '123') approve.approve_pw = 'OK';
+
+  res.send(approve);
+
 });
 
-WebSocket(server);
+app.listen(app.get('port'), ()=> {
+ console.log("익스프레스로 웹 서버를 실행함 : "+ app.get('port')); 
+});
